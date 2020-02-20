e0d5092e6886d879a41c6ec3c64b52a5623c16d4
Marek Bejda <marek.bejda@ibm.com>
2020-02-20 18:06:41 -0500

    with error catching

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-
 source utilities.sh
 
-slackMsg  "Dev" "SeaWayBill" "Version-1" "DocumentID12345"
+trap 'slackMsgFail "${LAST_STATUS}" $?' EXIT
+##Requires bootstrap script to pull in ci proj
+LAST_STATUS="Just finishd develop"
+
+slackMsg "Dev" "SeaWayBill" "Version-1" "DocumentID12345"

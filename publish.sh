fd5e096d13ab076c15cd43381d0b791665da93e1
Marek Bejda <marek.bejda@ibm.com>
2020-02-20 17:57:08 -0500

    ok done

--- publish.sh
+++ publish.sh
@@ -1,7 +1,3 @@
 source utilities.sh
 
-#trap 'slackMsgFail "${LAST_STATUS}" $?' EXIT
-##Requires bootstrap script to pull in ci proj
-#LAST_STATUS="Just finishd develop"
-
 slackMsg  "Dev" "SeaWayBill" "Version-1" "DocumentID12345"
